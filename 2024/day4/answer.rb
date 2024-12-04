b58ef5cd966aa7625e5bf019bf0dfdf16240260f
Mukhlas Rosyadi <mukhlasrosyadi@outlook.com>
2024-12-04 15:12:59 +0700

    day4: little cleanup

--- 2024/day4/answer.rb
+++ 2024/day4/answer.rb
@@ -1,4 +1,4 @@
-def count(line)
+def countxmas(line)
   return 0 if line.length < 4
   line.chars.each_cons(4).map(&:join).count { |word| word == "XMAS" || word == "SAMX" }
 end
@@ -29,40 +29,42 @@ word_search = f.each_line.map do |line|
   line
 end
 
+n = word_search.size - 1
+
 horizontal = word_search
 vertical = word_search.map(&:chars)
   .transpose
   .map(&:join)
 
 # [[0,0]], [[1,0], [0,1]], [[2,0], [1,1], [0,2]], [[3,0], [2,1], [1,2], [0,3]], [[4,0], [3,1], [2,2], [1,3], [0,4]]
-diagonal1 = (0 ... word_search.size).map do |i| 
+diagonal1 = (0 .. n).map do |i| 
   (0 .. i).map do |j|
     word_search[i-j].chars[j]
   end.join
 end
 
 # [[n, n]], [[n, n-1], [n-1, n]], [[n, n-2], [n-1, n-1], [n-2, n]], [[n, n-3], [n-1, n-2], [n-2, n-1], [n-3, n]], [[n, n-4], [n-1, n-3], [n-2, n-2], [n-3, n-1], [n-4, n]]
-diagonal2 = (0 ... word_search.size-1).map do |i|
+diagonal2 = (0 ... n).map do |i|
   (0 .. i).map do |j|
-    word_search[word_search.size - 1 - j].chars[word_search.size - 1 - i + j]
+    word_search[n - j].chars[n - i + j]
   end.join
 end
 
 # [[0,n]], [[0, n-1], [1, n]], [[0, n-2], [1, n-1], [2, n]], [[0, n-3], [1, n-2], [2, n-1], [3, n]], [[0, n-4], [1, n-3], [2, n-2], [3, n-1], [4, n]]
-diagonal3 = (0 ... word_search.size).map do |i|
+diagonal3 = (0 .. n).map do |i|
   (0 .. i).map do |j|
-    word_search[j].chars[word_search.size - 1 - i + j]
+    word_search[j].chars[n - i + j]
   end.join
 end
 
 # [[n,0]], [[n-1, 0], [n, 1]], [[n-2, 0], [n-1, 1], [n, 2]], [[n-3, 0], [n-2, 1], [n-1, 2], [n, 3]], [[n-4, 0], [n-3, 1], [n-2, 2], [n-1, 3], [n, 4]]
-diagonal4 = (0 ... word_search.size - 1).map do |i|
+diagonal4 = (0 ... n).map do |i|
   (0 .. i).map do |j|
-    word_search[word_search.size - 1 - i + j].chars[j]
+    word_search[n - i + j].chars[j]
   end.join
 end
 
-part1 = [horizontal, vertical, diagonal1, diagonal2, diagonal3, diagonal4].map { |lines| lines.map { |line| count(line) }.sum }.sum
+part1 = [horizontal, vertical, diagonal1, diagonal2, diagonal3, diagonal4].map { |lines| lines.map { |line| countxmas(line) }.sum }.sum
 
 puts "Part 1: #{part1}"
 puts "Part 2: #{count_x_mas(word_search)}"
